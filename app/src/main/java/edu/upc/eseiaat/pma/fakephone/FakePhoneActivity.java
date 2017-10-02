1dd104c47be28669c8f0279873055ae582fdbea4
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-02 15:43:53 +0200

    crear botó sense listener fent servir l'onclick

---
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -0,0 +1,21 @@
+package edu.upc.eseiaat.pma.fakephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class FakePhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_phone);
+    }
+
+    public void clicat (View v) {
+        Button boto = (Button) v;
+        Log.i("click", "Has clicat el botó!");
+    }
+}
