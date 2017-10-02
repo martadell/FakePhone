12e42a46bd3e29e03b4653e47636b61ac44fde67
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-02 16:52:50 +0200

    fer que aparegui el numero que es clica sobre el botó al textview

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -5,17 +5,33 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FakePhoneActivity extends AppCompatActivity {
+
+    TextView txt_numbers;
+    private String n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_phone);
+
+    txt_numbers = (TextView) findViewById(R.id.txt_numbers);}
+
+    public void clicnum (View v) {
+        Button btn = (Button) v;
+        String number = btn.getText().toString();
+        txt_numbers.setText(number);
+
     }
 
-    public void clicat (View v) {
+    /*public void clicat (View v) {
         Button boto = (Button) v;
         Log.i("click", "Has clicat el botó!");
-    }
+    }*/
+
+/*
+String n = "";
+    n = number + n;*/
 }
