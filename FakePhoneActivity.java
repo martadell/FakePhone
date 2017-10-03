14d2df5266893fd8758d1b732308f069bfa56c36
martadell <32375522+martadell@users.noreply.github.com>
2017-10-03 12:56:41 +0200

    Add files via upload
    afegir la resta de botons (els numeros funcionen) i provar d'esborrar-los

---
+++ FakePhoneActivity.java
@@ -0,0 +1,45 @@
+package edu.upc.eseiaat.pma.fakephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FakePhoneActivity extends AppCompatActivity {
+
+    TextView txt_numbers;
+    private String n;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_phone);
+
+    txt_numbers = (TextView) findViewById(R.id.txt_numbers);
+    n = "";}
+
+    public void clicnum (View v) {
+        Button btn = (Button) v;
+        String number = btn.getText().toString();
+        n = n + number;
+        txt_numbers.setText(n);
+    }
+
+    public void clicdel (View v) {
+        Button btn = (Button) v;
+        txt_numbers.setText("");
+    }
+
+  /*  public void clicall (View v) {
+        Button btn = (Button) v;
+        Toast.makeText(this, R.string.calling + txt_numbers.getText(), Toast.LENGTH_LONG).show();
+    }/*
+
+    /*public void clicat (View v) {
+        Button boto = (Button) v;
+        Log.i("click", "Has clicat el botó!");
+    }*/
+}
